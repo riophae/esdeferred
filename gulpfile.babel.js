02b560f6931a826cb1f748d374fcb1ae9a50ca94
Riophae Lee <riophaelee@gmail.com>
2015-08-02 03:13:37 +0800

    update: update build script

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,5 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
-import uglify from 'gulp-uglify'
 import del from 'del'
 
 gulp.task('clean', (cb) => {
@@ -10,7 +9,6 @@ gulp.task('clean', (cb) => {
 gulp.task('build', [ 'clean' ], () => {
   return gulp.src([ './src/*.js' ])
     .pipe(babel())
-    .pipe(uglify({ preserveComments: 'some' }))
     .pipe(gulp.dest('./lib'))
 })
 
