d3f6ef423c2b4cefb9e7705521f292bda5e91b88
李方舟 <lifangzhou@meituan.com>
2015-08-27 10:17:45 +0800

    update: DeferredAsync

--- src/deferred-async.js
+++ src/deferred-async.js
@@ -2,8 +2,14 @@ import { Deferred } from './deferred'
 
 class DeferredAsync extends Deferred {
   constructor (onFulfilled = Deferred.success, onRejected = Deferred.error) {
-    const onFulfilledAsync = DeferredAsync.success.bind(null, onFulfilled)
-    const onRejectedAsync = DeferredAsync.error.bind(null, onRejected)
+    const onFulfilledAsync = (val) => {
+      const d = Deferred.resolve(val)
+      return d.then(onFulfilled)
+    }
+    const onRejectedAsync = (err) => {
+      const d = Deferred.reject(err)
+      return d.catch(onRejected)
+    }
 
     super(onFulfilledAsync, onRejectedAsync)
   }
@@ -12,16 +18,6 @@ class DeferredAsync extends Deferred {
     this._next = new DeferredAsync(onFulfilled, onRejected)
     return this._next
   }
-}
-
-DeferredAsync.success = (onFulfilled, val) => {
-  const d = Deferred.resolve(val)
-  return d.then(onFulfilled)
-}
-
-DeferredAsync.error = (onRejected, err) => {
-  const d = Deferred.reject(err)
-  return d.catch(onRejected)
 }
 
 export { DeferredAsync }
