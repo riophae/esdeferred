060e3385a1f1fd20c310f7d958be13e04d43f86f
李方舟 <lifangzhou@meituan.com>
2015-09-07 10:07:40 +0800

    remove: useless utils

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,2 @@
 export const callAsync = typeof process !== 'undefined' && process.nextTick ?
   process.nextTick : setTimeout
-
-export const entries = function * (obj) {
-  for (const key of Object.keys(obj)) {
-    yield [ key, obj[key] ]
-  }
-}
-
-export const filterMap = function (obj, filterFn) {
-  const map = {}
-
-  for (const [ k, v ] of entries(obj)) {
-    if (filterFn(v, k)) {
-      map[k] = v
-    }
-  }
-
-  return map
-}
