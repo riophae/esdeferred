14f120fc9236ffbc2e66f71d158ccc9e5c4dde72
李方舟 <lifangzhou@meituan.com>
2015-09-07 10:32:08 +0800

    update: refactor `callAsync()`

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,28 @@
-export const callAsync = typeof process !== 'undefined' && process.nextTick ?
-  process.nextTick : setTimeout
+export const callAsync = (() => {
+  // https://github.com/isaacs/node-bench/blob/master/lib/bench.js
+
+  let nT, sI
+  const sT = (fn) => setTimeout(fn, 0)
+
+  if (typeof process !== 'undefined' && process && typeof process.nextTick === 'function') {
+    nT = process.nextTick
+  }
+
+  if (typeof setImmediate === 'function') {
+    sI = setImmediate
+  }
+
+  const _nt = sI || nT
+  if (!_nt) return sT
+
+  let _ntN = 0
+
+  return function callAsync (fn) {
+    if (_ntN++ < 100) {
+      nT(fn)
+    } else {
+      _nt(fn)
+      _ntN = 0
+    }
+  }
+})()
